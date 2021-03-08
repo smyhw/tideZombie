a5d0bf5b3d9d437d02bc912f2807c5ba99c6ce94
smyhw <i@smyhw.online>
2021-03-08 15:04:40 +0800

    修复夜晚尸潮错误

--- src/online/smyhw/tideZombie/triggers/OnNightStartTrigger.java
+++ src/online/smyhw/tideZombie/triggers/OnNightStartTrigger.java
@@ -33,16 +33,16 @@ class OnNightStartTriggerTask extends BukkitRunnable {
 
 	@Override
 	public void run() {
-		World wd = Bukkit.getWorld(Tz.configer.getString("triggers.OnNightStartTrigger.on_night_world", "world"));
+		World wd = Bukkit.getWorld(Tz.configer.getString("triggers."+triggerID+".on_night_world", "world"));
 		if(wd==null) {
-			Tz.loger.warning("用来确定时间的世界<"+Tz.configer.getString("triggers.OnNightStartTrigger.on_night_world", "没有找到on_night_world配置项")+">不存在");
+			Tz.loger.warning("用来确定时间的世界<"+Tz.configer.getString("triggers."+triggerID+".on_night_world", "没有找到on_night_world配置项")+">不存在");
 			this.cancel();
 		}
 		long time = wd.getFullTime();
 		if(time>24000) {//1.7.10以下，时间戳不会每天重置，而是一直向上叠加
 			time = time%24000;
 		}
-		if(time>14000 && time< 14030){
+		if(time>14000 && time<= 14020){
 			Tz.loger.info("夜晚自动开启尸潮");
 			new DoMob(Tz.thisPlugin,Tz.configer.getString("triggers."+triggerID+".targetTide"),"触发器<"+triggerID+">");
 		}
